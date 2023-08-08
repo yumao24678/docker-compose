df12d30ceea4dd365aa101d8529dc68168a3d546
yumao24678 <yumao24678@163.com>
2023-08-08 13:43:59 +0800

    docs: 删除无用注释

--- prometheus/init.sh
+++ prometheus/init.sh
@@ -5,9 +5,6 @@ set -eu
 cd "$(dirname "$0")"
 
 mkdir -p ./prometheus-data
-# mkdir -p ./alertmanager
-# chmod 777 ./prometheus-data
 chown -R 65534:65534 ./prometheus-data
-# chowm -R 65534:65534 ./alertmanager
 
 echo "Prometheus: Init Success !"
